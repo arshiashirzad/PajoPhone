7b6114e6eb72d0ee939a1966bdb51ca49f269492
arshiashirzad <sirzad383@gmail.com>
2024-05-30 10:44:36 +0330

    Added Models

---
+++ PajoPhone/Models/Category.cs
@@ -0,0 +1,7 @@
+namespace PajoPhone.Models;
+
+public class Category
+{
+        public int Id { get; set; }
+        public string Name { get; set; }
+}
---
+++ PajoPhone/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace PajoPhone.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Color { get; set; }
+    public byte[] Image { get; set; }
+    public double Price { get; set; }
+}
