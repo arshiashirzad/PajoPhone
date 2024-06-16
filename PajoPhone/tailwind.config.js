f2b2ed67710fe822f879f9b89f74642477258786
arshiashirzad <sirzad383@gmail.com>
2024-06-16 10:24:41 +0330

    added requried files

---
+++ PajoPhone/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './Views/**/*.cshtml',
+    './wwwroot/**/*.html',
+    './Pages/**/*.cshtml'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
